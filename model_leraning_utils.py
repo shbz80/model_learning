f9353cc74c0b687b59ab1eccc72d7c0940f91d3d
Shahbaz Khader <shahbazkhader@gmail.com>
2019-02-01 20:14:56 +0100

    added comments for UGP class

--- model_leraning_utils.py
+++ model_leraning_utils.py
@@ -1,11 +1,28 @@
 import numpy as np
 from scipy.linalg import cholesky
+'''
+usage:
 
+ugp_params = {
+    'alpha': 1.,
+    'kappa': 2.,
+    'beta': 0.,
+}
+ugp = UGP(dX + dU, **ugp_params)
+y_mu, y_var, _, _, xy_cor = ugp.get_posterior(gp, x_mu, x_var) 
+
+* gp should have predict method according to the scikit learn lib
+'''
 class UGP(object):
-    '''
-    This class assumes the multi dimensional GP in multidim_gp.py
-    '''
     def __init__(self, L, alpha=1e-3, kappa=0., beta=2.):
+        '''
+        Initialize the unscented transform parameters
+        :param L: dim of the input
+        a practical value set for the remaining params:
+        :param alpha: 1.
+        :param kappa: 2.
+        :param beta: 0.
+        '''
         self.L = L
         self.N = 2*L + 1
         self.alpha = alpha
@@ -14,6 +31,12 @@ class UGP(object):
 
 
     def get_sigma_points(self, mu, var):
+        '''
+        generate and return 2L+1 sigma points along with their weights
+        :param mu: mean of input
+        :param var: variance of output
+        :return:
+        '''
         L = self.L
         N = self.N
         assert(mu.shape == (L,))
@@ -32,8 +55,6 @@ class UGP(object):
         for i in range(1, L+1):
             sigmaMat[i, :] = mu + chol[:,i-1]
             sigmaMat[L+i, :] = mu - chol[:, i-1]
-            # sigmaMat[i, :] = mu + chol[i - 1, :]
-            # sigmaMat[L + i, :] = mu - chol[i - 1, :]
 
         W_mu = np.zeros(N)
         Wi = 1. / (2. * (L + Lambda))
@@ -46,24 +67,37 @@ class UGP(object):
         return sigmaMat, W_mu, W_var
 
     def get_posterior(self, fn, mu, var):
+        '''
+        Compute and return the output distribution along with the propagated sigma points
+        :param fn: the nonlinear function through which to propagate
+        :param mu: mean of the input
+        :param var: variance of the output
+        :return:
+                Y_mu_post: output mean
+                Y_var_post: output variance
+                Y_mu: transformed sigma points
+                Y_var: gp var for each transformed points
+                XY_cross_cov: cross covariance between input and output
+        '''
         sigmaMat, W_mu, W_var = self.get_sigma_points(mu, var)
-        Y_mu, Y_std = fn.predict(sigmaMat, return_std=True)
+        Y_mu, Y_std = fn.predict(sigmaMat, return_std=True) # same signature as the predict function of gpr but can be
+                                                            # any nonlinear function
         N, Do = Y_mu.shape
         Y_var = Y_std **2
         Y_var = Y_var.reshape(N,Do)
-        # Y_mu_post = np.average(Y_mu, axis=0, weights=W_mu)    # DX1
-        Y_mu_post = Y_mu[0]
+        Y_mu_post = np.average(Y_mu, axis=0, weights=W_mu)    # DX1
+        # Y_mu_post = Y_mu[0]
         Y_var_post = np.zeros((Do,Do))
-        # for i in range(N):
-        #    y = Y_mu[i] - Y_mu_post
-        #    yy_ = np.outer(y, y)
-        #    Y_var_post += W_var[i]*yy_
+        for i in range(N):
+           y = Y_mu[i] - Y_mu_post
+           yy_ = np.outer(y, y)
+           Y_var_post += W_var[i]*yy_
         #Y_var_post = np.diag(np.diag(Y_var_post))     # makes it worse
         Y_var_post += np.diag(Y_var[0])
         if Do == 1:
             Y_mu_post = np.asscalar(Y_mu_post)
             Y_var_post = np.asscalar(Y_var_post)
-        # ip op cross covariance
+        # compute cross covariance between input and output
         Di = mu.shape[0]
         XY_cross_cov = np.zeros((Di, Do))
         for i in range(N):
@@ -74,6 +108,13 @@ class UGP(object):
         return Y_mu_post, Y_var_post, Y_mu, Y_var, XY_cross_cov
 
     def get_posterior_pol(self, fn, mu, var):
+        '''
+        same as above but used for debugging
+        :param fn:
+        :param mu:
+        :param var:
+        :return:
+        '''
         sigmaMat, W_mu, W_var = self.get_sigma_points(mu, var)
         Y_mu, Y_std = fn.predict(sigmaMat, return_std=True)
         N, Do = Y_mu.shape
