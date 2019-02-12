1e8a8259f22d25acc63bc91f9cc43d3de65a0b02
Shahbaz Khader <shahbazkhader@gmail.com>
2019-02-12 15:31:05 +0100

    FUNCTIONALITY: Made dynamics of Blocks simulator stochastic. The noise variance is hardcoded now. Have to move it somewhere.

--- blocks_sim.py
+++ blocks_sim.py
@@ -78,8 +78,8 @@ class MassSlideWorld(object):
         X = X + dt*(1./6.)*(k1 + 2.*k2 + 2.*k3 + k4)
 
         self.t = self.t + dt
-        self.mode, self.X = self.step_mode(X, u) # TODO: restore this
-        # self.X = X
+        self.mode, self.X = self.step_mode(X, u)
+        self.X = np.random.normal(self.X, 1e-2) #TODO: the noise param has to be moved somewhere
         return self.t, self.X, self.mode
 
     def set_policy(self, policy_params):
