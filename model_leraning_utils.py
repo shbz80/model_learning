e89957da2141b4da3e35cbb21073dce76366e480
Shahbaz Khader <shahbazkhader@gmail.com>
2019-02-20 20:53:09 +0100

    FEATURE: Added a version of method to the class UGP for yumi exp case. THis method takes time index as a parameter. Should do refactor for function overloading.

--- model_leraning_utils.py
+++ model_leraning_utils.py
@@ -108,6 +108,48 @@ class UGP(object):
             XY_cross_cov += W_var[i] * xy_
         return Y_mu_post, Y_var_post, Y_mu, Y_var, XY_cross_cov
 
+    def get_posterior_time_indexed(self, fn, mu, var, t):
+        '''
+        Compute and return the output distribution along with the propagated sigma points
+        :param fn: the nonlinear function through which to propagate
+        :param mu: mean of the input
+        :param var: variance of the output
+        :return:
+                Y_mu_post: output mean
+                Y_var_post: output variance
+                Y_mu: transformed sigma points
+                Y_var: gp var for each transformed points
+                XY_cross_cov: cross covariance between input and output
+        '''
+        sigmaMat, W_mu, W_var = self.get_sigma_points(mu, var)
+        Y_mu, Y_std = fn.predict(sigmaMat, t, return_std=True) # same signature as the predict function of gpr but can be
+                                                            # any nonlinear function
+        N, Do = Y_mu.shape
+        Y_var = Y_std **2
+        Y_var = Y_var.reshape(N,Do)
+        Y_mu_post = np.average(Y_mu, axis=0, weights=W_mu)    # DX1
+        # Y_mu_post = Y_mu[0]
+        Y_var_post = np.zeros((Do,Do))
+        for i in range(N):
+           y = Y_mu[i] - Y_mu_post
+           yy_ = np.outer(y, y)
+           Y_var_post += W_var[i]*yy_
+        #Y_var_post = np.diag(np.diag(Y_var_post))     # makes it worse
+        Y_var_post += np.diag(Y_var[0])
+        if Do == 1:
+            Y_mu_post = np.asscalar(Y_mu_post)
+            Y_var_post = np.asscalar(Y_var_post)
+        # compute cross covariance between input and output
+        Di = mu.shape[0]
+        XY_cross_cov = np.zeros((Di, Do))
+        #TODO: XY_cross_cov may be wrong
+        for i in range(N):
+            y = Y_mu[i] - Y_mu_post
+            x = sigmaMat[i] - mu
+            xy_ = np.outer(x, y)
+            XY_cross_cov += W_var[i] * xy_
+        return Y_mu_post, Y_var_post, Y_mu, Y_var, XY_cross_cov
+
     def get_posterior_pol(self, fn, mu, var):
         '''
         same as above but used for debugging
